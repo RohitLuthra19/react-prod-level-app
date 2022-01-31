3d6d61b811dac3d776548ce1eb6d17a7c8da39bd
Rohit <rohit.luthra19@gmail.com>
2022-01-31 22:57:14 +0530

    fixed unit test cases and css

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App contains correct text', () => {
-  render(<App />);
+  test('should render successfully', () => {
+    const { getByTestId } = render(<App message="dummy-message" />);
+    expect(getByTestId('dummy-message')).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import './App.css';
 import * as React from 'react';
+import './App.css';
 
 type AppProps = {
   message?: string;
 };
 
 function App({ message }: AppProps) {
-  return <div>{message}</div>;
+  return (
+    <div className="class1" data-testid={message}>
+      {message}
+    </div>
+  );
 }
 
 App.defaultProps = {
-  message: '',
+  message: 'Hello',
 };
 
 export default App;
